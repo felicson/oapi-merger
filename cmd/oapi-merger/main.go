00ab4006c57f917b1769e873e83e9c8b7b096ff1
Sergey Belskiy <belskiy.s@bookcentre.ru>
2022-05-30 20:08:09 +0700

    Use internal ref name resolver

--- cmd/oapi-merger/main.go
+++ cmd/oapi-merger/main.go
@@ -4,14 +4,11 @@ import (
 	"context"
 	"flag"
 	"fmt"
+	"github.com/getkin/kin-openapi/openapi3"
 	"io/ioutil"
 	"log"
 	"net/url"
 	"os"
-	"path/filepath"
-	"strings"
-
-	"github.com/getkin/kin-openapi/openapi3"
 )
 
 var (
@@ -26,31 +23,29 @@ func init() {
 
 func main() {
 	flag.Parse()
+
 	if err := os.Chdir(wdir); err != nil {
 		log.Fatal(err)
 	}
+
 	loader := openapi3.NewLoader()
 	loader.IsExternalRefsAllowed = true
 	loader.ReadFromURIFunc = func(loader *openapi3.Loader, uri *url.URL) ([]byte, error) {
 		return ioutil.ReadFile(uri.Path)
 	}
-	doc, err := loader.LoadFromFile(filepath.Join(spec)) // i.e "api/openapi.yaml")
+	doc, err := loader.LoadFromFile(spec) // i.e "api/openapi.yaml")
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	// validate before merging
 	if err = doc.Validate(loader.Context); err != nil {
 		log.Fatal(err)
 	}
 
-	doc.InternalizeRefs(context.Background(), func(ref string) string {
-		if idx := strings.Index(ref, "#"); idx > 1 {
-			ref = ref[idx+1:]
-			return ref[strings.LastIndex(ref, "/")+1:]
-		}
-		return strings.TrimRight(filepath.Base(ref), filepath.Ext(ref))
-	})
+	doc.InternalizeRefs(context.Background(), nil)
 
+	// validate after merging
 	if err = doc.Validate(loader.Context); err != nil {
 		log.Fatal(err)
 	}
